c84a1aef7f6714795e172d7a1fae54c35599c640
Stéphane Senart <stephane.senart@gmail.com>
2021-07-21 19:03:46 +0200

    [PresenceCamera] Permit to stream images even if cameras are off.

--- netatmo/camera.py
+++ netatmo/camera.py
@@ -160,19 +160,29 @@ class NetatmoCamera(NetatmoBase, Camera):
         if not data.get("camera_id"):
             return
 
+        _LOGGER.info("handle_event: %s", data)
+
         if data["home_id"] == self._home_id and data["camera_id"] == self._id:
-            if data[WEBHOOK_PUSH_TYPE] in ["NACamera-off", "NACamera-disconnection"]:
+            if data[WEBHOOK_PUSH_TYPE] in ["NOC-disconnection", "NACamera-disconnection"]:
                 self.is_streaming = False
-                self._status = "off"
-            elif data[WEBHOOK_PUSH_TYPE] in [
-                "NACamera-on",
-                WEBHOOK_NACAMERA_CONNECTION,
-            ]:
+                self._alim_status = "off"
+            elif data[WEBHOOK_PUSH_TYPE] in ["NOC-connection", "NACamera-connection"]:
+                self.is_streaming = True
+                self._alim_status = "on"
+            elif data[WEBHOOK_PUSH_TYPE] in ["NACamera-on"]:
                 self.is_streaming = True
                 self._status = "on"
+            elif data[WEBHOOK_PUSH_TYPE] in ["NACamera-off"]:
+                self.is_streaming = False
+                self._status = "off"
+            elif data[WEBHOOK_PUSH_TYPE] in ["NOC-on"]:
+                self.is_streaming = True
+                self._status = "on"
+            elif data[WEBHOOK_PUSH_TYPE] in ["NOC-off"]:
+                self.is_streaming = False
+                self._status = "off"        
             elif data[WEBHOOK_PUSH_TYPE] == WEBHOOK_LIGHT_MODE:
-                self._light_state = data["sub_type"]
-
+                self._light_state = data["sub_type"]                
             self.async_write_ha_state()
             return
 
@@ -262,7 +272,7 @@ class NetatmoCamera(NetatmoBase, Camera):
         self._sd_status = camera.get("sd_status")
         self._alim_status = camera.get("alim_status")
         self._is_local = camera.get("is_local")
-        self.is_streaming = bool(self._status == "on")
+        self.is_streaming = bool(self._alim_status == "on") if self._model == "NOC" else bool(self._status == "on")
 
         if self._model == "NACamera":  # Smart Indoor Camera
             self.hass.data[DOMAIN][DATA_EVENTS][self._id] = self.process_events(
